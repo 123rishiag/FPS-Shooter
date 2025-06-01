bce39c13b4c1d2a263be5e041d29f82db8d58c15
Rishabh Garg <narishabh.garg@gmail.com>
2025-06-01 19:17:53 +0530

    Created Player Input Controls

--- Source/FPSShooter/Private/GameModes/FPSGameModeBase.cpp
+++ Source/FPSShooter/Private/GameModes/FPSGameModeBase.cpp
@@ -2,4 +2,11 @@
 
 
 #include "GameModes/FPSGameModeBase.h"
+#include "Characters/Player/FPSPlayer.h"
+#include "Characters/Player/FPSPlayerController.h"
 
+AFPSGameModeBase::AFPSGameModeBase()
+{
+    DefaultPawnClass = AFPSPlayer::StaticClass();
+    PlayerControllerClass = AFPSPlayerController::StaticClass();
+}
--- Source/FPSShooter/Public/GameModes/FPSGameModeBase.h
+++ Source/FPSShooter/Public/GameModes/FPSGameModeBase.h
@@ -13,5 +13,7 @@ UCLASS()
 class FPSSHOOTER_API AFPSGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	AFPSGameModeBase();
 };
